6e00a322618e871fce3930aa9da89323029a1d03
MunyinSam <kelvinsam233@gmail.com>
2025-12-29 19:43:23 +0700

    29/12/2025

--- medium/7/main.py
+++ medium/7/main.py
@@ -13,9 +13,12 @@ class Solution(object):
             r *= 10
             r += x % 10
             x //= 10
-        return r * negative
+
+        if pow(-2, 31) <= r * negative <= pow(2, 31) - 1:
+            return r * negative
+        return 0
             
 
 s = Solution()
-answer = s.reverse(120)
+answer = s.reverse(1534236469)
 print(answer)
